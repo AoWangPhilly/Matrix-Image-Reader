b30bf2d0d66eec1c26ef4058ef9b2f57c54d1849
AoWangPhilly <aowangphilly@gmail.com>
2021-03-21 12:37:01 -0400

    Take in model and read in

--- src/read_matrix.py
+++ src/read_matrix.py
@@ -1,54 +1,14 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-# def sort_contours(cnts, method="top-to-bottom"):
-# 	# initialize the reverse flag and sort index
-# 	reverse = False
-# 	i = 0
-# 	# handle if we need to sort in reverse
-# 	if method == "right-to-left" or method == "bottom-to-top":
-# 		reverse = True
-# 	# handle if we are sorting against the y-coordinate rather than
-# 	# the x-coordinate of the bounding box
-# 	if method == "top-to-bottom" or method == "bottom-to-top":
-# 		i = 1
-# 	# construct the list of bounding boxes and sort them from top to
-# 	# bottom
-# 	boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-# 	(cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
-# 		key=lambda b:b[1][i], reverse=reverse))
-# 	# return the list of sorted contours and bounding boxes
-# 	return (cnts, boundingBoxes)
 
 
-def rescaleFrame(frame, scale=0.75):
-    # Works with images, videos, and live videos
-    width = int(frame.shape[1] * scale)
-    height = int(frame.shape[0] * scale)
-
-    # Return frame and dimensions
-    return cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
-
-
-def clean_image(fname: str = 'Pictures/m1.png'):
-    img = cv2.imread(fname)
-    kernel = np.ones((7, 7), np.uint8)
-    img = cv2.dilate(img, kernel, iterations=1)
-    img = cv2.erode(img, kernel, iterations=1)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # gray = cv2.morphologyEx(gray, cv2.MORPH_OPEN, np.ones((4, 4), dtype=np.uint8)) # Perform noise filtering
-    blur = cv2.GaussianBlur(src=gray, ksize=(7, 7), sigmaX=1)
-    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                   cv2.THRESH_BINARY, 199, 25)
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-
-    thresh = 255 * (thresh < 128).astype(np.uint8)
-    coords = cv2.findNonZero(thresh)
-    x, y, w, h = cv2.boundingRect(coords)  # Find minimum spanning bounding box
-    rect = thresh[y:y+h, x:x+w]
-    rect = rescaleFrame(rect, .5)
-    cv2.imshow('rect', rect)
-    cv2.waitKey(0)
+def predict_digit(digit, dim=(28, 28)):
+    model = tf.keras.models.load_model('digit.model')
+    digit = cv2.resize(digit, dim)
+    digit = np.reshape(digit, (1, 28, 28))
+    prediction = model.predict(digit)
+    return np.argmax(prediction)
 
 
 def draw_borders(fname: str = 'Pictures/m1.png'):
